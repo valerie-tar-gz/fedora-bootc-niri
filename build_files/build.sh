63d019a5c547a3d85e98be00c8069ab95c1d4d0f
Valerie <155010216+valerie-tar-gz@users.noreply.github.com>
2025-09-17 16:28:53 +0100

    Add basic Niri packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,14 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+#Enable COPRs
+dnf5 -y copr enable yalter/niri
+
+# Install packages
+dnf5 install -y gdm niri flatpak
+
+#Disable COPRs
+dnf5 -y copr disable yalter/niri
 
 # Use a COPR Example:
 #
